cad2d3048175aae938cfa7897e3ee6c6261850a2
robinBanquo <caramaschi@hotmail.com>
2017-06-01 16:28:28 +0200

    premier exo OK!

--- src/BlogBundle/Controller/DefaultController.php
+++ src/BlogBundle/Controller/DefaultController.php
@@ -2,18 +2,70 @@
 
 namespace BlogBundle\Controller;
 
+use BlogBundle\Entity\Post;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('BlogBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+        $repository = $em->getRepository('BlogBundle:Post');
+        $listPosts = $repository->findAll();
+
+
+        return $this->render('BlogBundle:Default:index.html.twig', array(
+            "listPosts" => $listPosts,
+        ));
     }
 
-    public function addAction()
+
+
+
+
+    public function addAction(Request $request)
     {
-        return $this->render('BlogBundle:Default:add.html.twig');
+        $post = new Post();
+
+        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $post);
+
+        $formBuilder
+            ->add('title',     TextType::class)
+            ->add('content',   TextareaType::class)
+            ->add('save',      SubmitType::class)
+        ;
+
+        $form = $formBuilder->getForm();
+
+        if ($request->isMethod('POST')) {
+
+
+            $form->handleRequest($request);
+            // On vérifie que les valeurs entrées sont correctes
+
+
+            // On enregistre notre objet $advert dans la base de données, par exemple
+
+            $em = $this->getDoctrine()->getManager();
+
+            $em->persist($post);
+
+            $em->flush();
+
+            $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+            return $this->redirectToRoute('blog_homepage');
+        }
+            return $this->render('BlogBundle:Default:add.html.twig', array(
+            'form' =>$form->createview(),
+        ));
     }
 }
