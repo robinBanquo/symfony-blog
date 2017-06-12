29089d057add2671726fa74a2bf5264ec1e339c5
robinBanquo <caramaschi@hotmail.com>
2017-06-12 11:30:01 +0200

    premier exo

---
+++ src/BlogBundle/Form/ImageType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace BlogBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ImageType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('url');
+    }
+    
+    /**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'BlogBundle\Entity\Image'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'blogbundle_image';
+    }
+
+
+}
