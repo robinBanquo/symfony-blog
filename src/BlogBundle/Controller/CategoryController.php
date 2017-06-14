9fb394f1af5d80754b8c036721135277821b2b00
robinBanquo <caramaschi@hotmail.com>
2017-06-14 14:56:32 +0200

    avec tab =2 ca marche pas

--- src/BlogBundle/Controller/CategoryController.php
+++ src/BlogBundle/Controller/CategoryController.php
@@ -10,10 +10,7 @@ namespace BlogBundle\Controller;
 
 use BlogBundle\Entity\Category;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
+
 use Symfony\Component\HttpFoundation\Request;
 class CategoryController extends Controller
 {
